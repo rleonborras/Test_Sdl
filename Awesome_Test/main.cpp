47a3116f18f447c09faca519958077e5290d72bb
Roger Leon Borras <36153923+rleonborras@users.noreply.github.com>
2018-02-28 12:09:24 +0100

    Update_Project

---
+++ Awesome_Test/main.cpp
@@ -0,0 +1,12 @@
+#include "SDL\include\SDL.h"
+#pragma comment(lib,"SDL/libx86/SDL2.lib")
+#pragma comment(lib,"SDL/libx86/SDL2main.lib")
+
+
+int main(int argc, char* argv[]) {
+
+
+
+
+	return 0;
+}
