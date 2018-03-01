66045622d42c35dae5e617dbffa07157f4b3dfdb
Roger Leon Borras <36153923+rleonborras@users.noreply.github.com>
2018-03-01 14:28:05 +0100

    New_Exercise

--- Awesome_Test/main.cpp
+++ Awesome_Test/main.cpp
@@ -12,10 +12,10 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-SDL_Window* window = NULL;
-SDL_Surface* screenSurface ;
-using namespace std;
+SDL_Window* window = nullptr;
+SDL_Surface* screenSurface =nullptr;
 
+SDL_KeyboardEvent Button;
 
 bool init() {
 
@@ -26,14 +26,14 @@ bool init() {
 
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		cout << "This doesn't work" << endl;
+		printf( "This doesn't work" );
 		succes = false;
 	}
 	else {
 		//Create Window(this only creates the "marco" of the windows, here we cannot draw anything)
 		window = SDL_CreateWindow("AWESOME GAME", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT,SDL_WINDOW_SHOWN);
 		if (window == NULL) {
-			cout << "Window could not be created! SDL_ERROR: " << SDL_GetError() << endl;
+			printf(" Window could not be created! SDL_ERROR: %s \n", SDL_GetError());
 			succes = false;
 		}
 		else {
@@ -49,6 +49,7 @@ bool init() {
 void close() {
 	//Destroy window
 	SDL_DestroyWindow(window);
+
 	//Quit from SDL subsystems
 	SDL_Quit();
 }
@@ -59,8 +60,13 @@ int main(int argc, char* argv[]) {
 	bool quit = false;
 	bool collx = false;
 	bool colly = false;
+	//DETECTION CONTROLS
+	bool UP = false;
+	bool DOWN = false;
+	bool LEFT = false;
+	bool RIGHT = false;
 	int colorVar = 0;
-	int nosame;
+	int nosameColor;
 	SDL_Event e;
 	Uint32 rectColor;
 	Uint32 rectColorPickup;
@@ -76,10 +82,11 @@ int main(int argc, char* argv[]) {
 	rectPickup.w = 30;
 	rectPickup.h = 30;
 	//handle events on queue
+	const Uint8 *state = SDL_GetKeyboardState(NULL);
 
 		//start up SDL and create a window
 	if (!init()) {
-		cout << "Failed to initialize" << endl;
+		
 	}
 	else {
 		//fill the surface blue
@@ -91,7 +98,7 @@ int main(int argc, char* argv[]) {
 		SDL_FillRect(screenSurface, &rect, rectColor);
 	}
 
-	while(!quit){
+	while (!quit) {
 
 		while (SDL_PollEvent(&e) != 0) // This gets an event so this will happens until SDLQUIT(in this case pres the x button) happens)
 		{
@@ -99,99 +106,83 @@ int main(int argc, char* argv[]) {
 			{
 				quit = true;
 			}
-		}
-		while (rect.x < 640||rect.x>0||rect.y<480||rect.y>0) {  //Delimitation zone
-			
-			//Change the color of the pickups and the Main block and respawn.
-			for (int i = rect.x; i < rect.x + 150; ++i) {
-				for (int j = rect.y; j < rect.y + 150; ++j) {
-					if ((i >= rectPickup.x && i <= rectPickup.x + rectPickup.w) && (j >= rectPickup.y && j <= rectPickup.y + rectPickup.h)) {
-						rectColor = rectColorPickup;
-						rectPickup.x = rand() % 500 + 20;
-						rectPickup.y = rand() % 300 + 20;
-						nosame = colorVar;
-						colorVar = rand() % 10;
-						while (colorVar == nosame) {
-							colorVar = rand() % 10;
-						}
-						if (colorVar == 0) {
-							rectColorPickup = SDL_MapRGB(screenSurface->format, RED);
-						}
-						else if (colorVar == 1) {
-							rectColorPickup = SDL_MapRGB(screenSurface->format, BLACK);
-						}	
-						else if (colorVar == 2) {
-							rectColorPickup = SDL_MapRGB(screenSurface->format, WHITE);
-						}
-						else if (colorVar == 3) {
-							rectColorPickup = SDL_MapRGB(screenSurface->format, YELLOW);
-						}
-						else if (colorVar == 4) {
-							rectColorPickup = SDL_MapRGB(screenSurface->format, GREEN);
-						}
-						else if (colorVar == 5) {
-							rectColorPickup = SDL_MapRGB(screenSurface->format, MAGENTA);
-						}
-						else if (colorVar == 6) {
-							rectColorPickup = SDL_MapRGB(screenSurface->format, CYAN);
-						}
-						else if (colorVar == 7) {
-							rectColorPickup = SDL_MapRGB(screenSurface->format, PINK);
-						}
-						else if (colorVar == 8) {
-							rectColorPickup = SDL_MapRGB(screenSurface->format, BROWN);
-						}
-						else if (colorVar == 9) {
-							rectColorPickup = SDL_MapRGB(screenSurface->format, ORANGE);
-						}	
-					
-						break;
-					}
+			//User Prees a key;
+		
+				//while (rect.x < 640 || rect.x>0 || rect.y < 480 || rect.y>0) {  //Delimitation zone
+				
+				if (state[SDL_SCANCODE_A]) {
+					rect.x-= 8;
 				}
+				else if (state[SDL_SCANCODE_W]) {
+					rect.y -= 8;
+				}
+				else if (state[SDL_SCANCODE_D]) {
+					rect.x += 8;
+				}
+				else if (state[SDL_SCANCODE_S]) {
+					rect.y += 8;
+				}
+			
 		}
+		//Updating the windows surface.
+		SDL_UpdateWindowSurface(window);
+		SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, BLUE));
+		SDL_FillRect(screenSurface, &rect, rectColor);
+		SDL_FillRect(screenSurface, &rectPickup, rectColorPickup);
+		SDL_Delay(1);
+	}
+close();
 
-			// the movement in X direction.
-			// the collX is the boolean variable to determine if the box has reached the end and then change the direction.
-			if (rect.x < 490 && (collx==false)) { 
-				rect.x++;
-			}
-			else if (rect.x >= 490) {
-				collx = true;
-				rect.x--;
-			}
-			else if(rect.x>0&&(collx==true)){
-				rect.x--;
-			}
-			else if (rect.x <=0) {
-				rect.x++;
-				collx = false;
-			}
-
-			// The Y direction update
-			if (rect.y < 330 && (colly == false)) {
-				rect.y++;
-			}
-			else if (rect.y >= 330) {
-				colly = true;
-				rect.y--;
-			}
-			else if (rect.y>0 && (colly == true)) {
-				rect.y--;
-			}
-			else if (rect.y <= 0) {
-				rect.y++;
-				colly = false;
-			}
-
-			//Updating the windows surface.
-			SDL_UpdateWindowSurface(window);
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, BLUE));
-			SDL_FillRect(screenSurface, &rect, rectColor);
-			SDL_FillRect(screenSurface, &rectPickup, rectColorPickup);
-			SDL_Delay(1);			
-		}
-	}	
-	close();
-
-	return 0;
+return 0;
 }
+	 
+
+// Change the color of the pickups and the Main block and respawn.
+/*for (int i = rect.x; i < rect.x + 150; ++i) {
+for (int j = rect.y; j < rect.y + 150; ++j) {
+if ((i >= rectPickup.x && i <= rectPickup.x + rectPickup.w) && (j >= rectPickup.y && j <= rectPickup.y + rectPickup.h)) {
+rectColor = rectColorPickup;
+rectPickup.x = rand() % 500 + 20;
+rectPickup.y = rand() % 300 + 20;
+nosame = colorVar;
+colorVar = rand() % 10;
+while (colorVar == nosame) {
+colorVar = rand() % 10;
+}
+if (colorVar == 0) {
+rectColorPickup = SDL_MapRGB(screenSurface->format, RED);
+}
+else if (colorVar == 1) {
+rectColorPickup = SDL_MapRGB(screenSurface->format, BLACK);
+}
+else if (colorVar == 2) {
+rectColorPickup = SDL_MapRGB(screenSurface->format, WHITE);
+}
+else if (colorVar == 3) {
+rectColorPickup = SDL_MapRGB(screenSurface->format, YELLOW);
+}
+else if (colorVar == 4) {
+rectColorPickup = SDL_MapRGB(screenSurface->format, GREEN);
+}
+else if (colorVar == 5) {
+rectColorPickup = SDL_MapRGB(screenSurface->format, MAGENTA);
+}
+else if (colorVar == 6) {
+rectColorPickup = SDL_MapRGB(screenSurface->format, CYAN);
+}
+else if (colorVar == 7) {
+rectColorPickup = SDL_MapRGB(screenSurface->format, PINK);
+}
+else if (colorVar == 8) {
+rectColorPickup = SDL_MapRGB(screenSurface->format, BROWN);
+}
+else if (colorVar == 9) {
+rectColorPickup = SDL_MapRGB(screenSurface->format, ORANGE);
+}
+
+break;
+}
+}
+}*/
+
+
